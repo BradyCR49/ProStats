b45e02b5d42aa8863ecde02a698a8bc1561fddf9
BradyCR49 <70081844+BradyCR49@users.noreply.github.com>
2022-08-28 20:48:22 -0400

    Initial Commit

---
+++ ProStats/ProStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProStatsApp.swift
+//  ProStats
+//
+//  Created by Brady Rhodes on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ProStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
